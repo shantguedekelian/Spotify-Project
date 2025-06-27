7481eccd6064a656094aea64601f144b8304e9c4
Shant <sguedekelian@ucsd.edu>
2025-06-27 10:18:09 -0700

    hosting app

--- app.py
+++ app.py
@@ -25,4 +25,4 @@ app.layout = html.Div([
 #     app.run(debug=True)
     
 if __name__ == "__main__":
-    app.run_server(host='0.0.0.0', port=8080, debug=False)
+    app.run(host='0.0.0.0', port=8080, debug=False)
